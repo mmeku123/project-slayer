21c386bb599f78f2045081184f98e3a843015313
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2019-04-05 14:43:32 +0700

    added material ui and antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
+import 'antd/dist/antd.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
